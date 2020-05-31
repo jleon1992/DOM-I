3437eed8ae1f142823ddef70f4782db0e8790367
jleon1992 <36936276+jleon1992@users.noreply.github.com>
2020-05-31 19:30:32 -0400

    filled in page with content from the JSON using the DOM with Javascript

--- js/index.js
+++ js/index.js
@@ -40,3 +40,68 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Nav Items
+const navItem = document.querySelectorAll('nav a');
+navItem[0].textContent = siteContent["nav"]["nav-item-1"];
+navItem[1].textContent = siteContent["nav"]["nav-item-2"];
+navItem[2].textContent = siteContent["nav"]["nav-item-3"];
+navItem[3].textContent = siteContent["nav"]["nav-item-4"];
+navItem[4].textContent = siteContent["nav"]["nav-item-5"];
+navItem[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+
+// Header
+const heading = document.querySelector('.cta-text h1');
+heading.innerHTML = siteContent["cta"]["h1"];
+
+const button = document.querySelector('button');
+button.innerHTML = siteContent["cta"]["button"];
+
+const code = document.querySelector('#cta-img');
+code.src = siteContent["cta"]["img-src"];
+
+// Mid-Content
+const midPageImg = document.querySelector('#middle-img');
+midPageImg.src = siteContent["main-content"]["middle-img-src"];
+
+const textBox = document.querySelectorAll('.text-content h4');
+
+textBox[0].innerHTML = siteContent["main-content"]["features-h4"];
+
+textBox[1].innerHTML = siteContent["main-content"]["about-h4"];
+
+textBox[2].innerHTML = siteContent["main-content"]["services-h4"];
+
+textBox[3].innerHTML = siteContent["main-content"]["product-h4"];
+
+textBox[4].innerHTML = siteContent["main-content"]["vision-h4"];
+
+
+const description = document.querySelectorAll('.text-content p');
+description[0].innerHTML = siteContent["main-content"]["features-content"];
+description[1].innerHTML = siteContent["main-content"]["about-content"];
+description[2].innerHTML = siteContent["main-content"]["services-content"];
+description[3].innerHTML = siteContent["main-content"]["product-content"];
+description[4].innerHTML = siteContent["main-content"]["vision-content"];
+
+
+// Contact
+
+const contactHead = document.querySelector('.contact h4');
+contactHead.textContent = siteContent["contact"]["contact-h4"];
+
+
+
+
+
+const contactInfo = document.querySelectorAll('.contact p');
+contactInfo[0].innerHTML = siteContent["contact"]["address"]; 
+contactInfo[1].innerHTML = siteContent["contact"]["phone"]; 
+contactInfo[2].innerHTML = siteContent["contact"]["email"]; 
+
+// Footer
+const copy = document.querySelector('footer p');
+copy.innerHTML = siteContent["footer"]["copyright"];
