9cdee84e890cfff324f7ffa7c430485ac8b0d8d8
jleon1992 <36936276+jleon1992@users.noreply.github.com>
2020-05-31 21:31:30 -0400

    renamed variables and functions, organized and wrote comments

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,40 +1,38 @@
-const msten = document.querySelector('#msTens');
+const msTen = document.querySelector('#msTens');
 const msHundreds = document.querySelector('#msHundreds');
 const secOnes = document.querySelector('#secondOnes');
 const secTens = document.querySelector('#secondTens');
 const digit = document.querySelector('.digits');
 
-
+// Variables
 let count = 1;
-var zero = 1;
+let zero = 1;
 let one = 1;
 let ten = 1;
+// Intervals
+var toTen = setInterval(tens, 10);
+
+// To stop the interval counter
+function stop() {
+    digit.style.color = "red";
+    clearInterval(toTen);
+}
 
 
-// function hundreds(){
-    
-//     msHundreds.innerHTML = count;
-//     count += 1;
-//     if(count === 10){
-//         count = 0;
-//     }
-// };
-
-
-
-    
-function myTimer(){
+// Timer is 10ms increments
+function tens(){
     if(zero === 10){
         zero = 0;
-        msten.innerHTML = zero;
+        msTen.innerHTML = zero;
         zero += 1; 
         hundreds();
    }else{
-    msten.innerHTML = zero;
+    msTen.innerHTML = zero;
     zero += 1; 
    }    
 }
 
+// Timer in 100ms increments
 function hundreds(){
     if(count === 10){
         count = 0;
@@ -47,14 +45,14 @@ function hundreds(){
    }    
 }
 
+// Timer by the second
 function ones(){
     if(one === 10){
         one = 0;
         secOnes.innerHTML = one;
         one += 1; 
-        tens();
+        secTens.innerHTML = 1;
         stop();
-
    }else{
     secOnes.innerHTML = one;
     one += 1; 
@@ -62,15 +60,3 @@ function ones(){
 }
 
 
-function tens(){
-
-    secTens.innerHTML = ten;
-    ten += 1; 
-    
-}
-
-  var toTen = setInterval(myTimer, 10);
-  function stop() {
-      digit.style.color = "red";
-    clearInterval(toTen);
-  }
